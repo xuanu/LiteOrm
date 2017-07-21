aa7a9919b2df8ad0e48331c0d5bd9c2bbc061054
zeffect <625417632@qq.com>
2017-07-21 15:05:26 +0800

    去掉不用的

---
+++ app/src/main/java/utils/cn/zeffect/liteorm/MainActivity.java
@@ -0,0 +1,14 @@
+package utils.cn.zeffect.liteorm;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+import com.litesuits.orm.LiteOrm;
+
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
